3a38ead189c0c6639aada13b7b2aa0b619fd4079
Slope <60516263+Slope86@users.noreply.github.com>
2022-12-09 00:20:45 +0800

    [add] document

--- argument.py
+++ argument.py
@@ -1,9 +1,26 @@
-# Description: Parse arguments from command line
+# Description: Parse arguments from command line.
 import argparse
 
 
 class Argument:
-    """Parse arguments from command line."""
+    """
+    Parse arguments from command line.
+
+    This class uses the `argparse` module to parse arguments passed to the program on the command line.
+    It defines two optional arguments:
+
+    - "-d" or "--delay": Set the delay between Google search requests. The default value is 1.
+    - "-np" or "--no-print": Do not print the QA process.
+
+    The parsed arguments are saved as attributes of the class, and can be accessed by other parts of the program.
+
+    This class also uses the Singleton pattern to ensure that only one instance of ArgumentParser is created.
+
+    Example:
+        args = Argument()
+        delay = args.delay
+        should_print = args.print
+    """
 
     def __new__(cls):
         # Singleton
@@ -16,10 +33,12 @@ class Argument:
         parser.add_argument(
             "-d", "--delay", type=int, default=1, help="Delay between Google search requests, default = 1"
         )
-        parser.add_argument("-np", "--no-print", action="store_false", dest="print", help="don't print the QA process")
+        parser.add_argument(
+            "-np", "--no-print", action="store_false", dest="print", help="Will not print the QA process"
+        )
         args = parser.parse_args()
 
-        # Save arguments in attributes
+        # Save arguments as attributes
         cls.delay: int = args.delay
         cls.print: bool = args.print
         return cls._instance
